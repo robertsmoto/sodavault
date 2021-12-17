3c8755fbcf8a5253e1433536a14f65f828a4c9a8
Scott Roberts <roberts.moto@gmail.com>
2021-12-17 17:23:36 +0100

    setting up dynamic api host

--- src/sodavault/hosts.py
+++ src/sodavault/hosts.py
@@ -8,7 +8,7 @@ API_HOST = config('ENV_API_HOST')
 host_patterns = patterns(
         '',
         host(r'', 'sodavault.urls', name='default-host'),
-        host(r'api-stage', 'graphqlapp.urls', name='api'),
+        host(r'%s', 'graphqlapp.urls', name='api') % (API_HOST),
         host(r'blog', 'blogapp.urls', name='blog'),
         host(r'auth', 'registration.auth_urls', name='auth'),
         host(
