fb1178e1b046abc2c32538b7d88f5afd1c5cc86d
Scott Roberts <roberts.moto@gmail.com>
2021-12-17 17:08:46 +0100

    setting up dynamic api host

--- src/sodavault/hosts.py
+++ src/sodavault/hosts.py
@@ -1,27 +1,15 @@
-from django.contrib import admin
-from django.conf import settings
 from django_hosts import patterns, host
+from decouple import config
 
+DEBUG = config('ENV_DEBUG', default=False, cast=bool)
 
-host_patterns = patterns('',
-    host(r'', 'sodavault.urls', name='default-host'), 
-    host(r'api', 'graphqlapp.urls', name='api'),
-    host(r'blog', 'blogapp.urls', name='blog'),
-    host(r'auth', 'registration.auth_urls', name='auth'),
-    host(r'accounts', 'registration.backends.default.urls', name='accounts'),
+API_HOST = config('ENV_API_HOST')
+
+host_patterns = patterns(
+        '',
+        host(r'', 'sodavault.urls', name='default-host'),
+        host(rf'{API_HOST}', 'graphqlapp.urls', name='api'),
+        host(r'blog', 'blogapp.urls', name='blog'),
+        host(r'auth', 'registration.auth_urls', name='auth'),
+        host(r'accounts', 'registration.backends.default.urls', name='accounts'),
 )
-
-
-"""
-use auth.sodavault.com for authorization views
-Another URLConf is also provided – at registration.auth_urls – which just handles the Django auth views, should you want to put those at a different location.
-
-
-
-
-host_patterns = patterns('path.to',
-            host(r'www', 'urls.default', name='default'),
-            host(r'api', 'urls.api', name='api'),
-            host(r'admin', 'urls.admin', name='admin', scheme='https://'),
-        )
-"""
