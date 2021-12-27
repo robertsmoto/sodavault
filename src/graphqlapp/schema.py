0b6a2ca3a1d806f36af607c72f7589ecaf94f7f3
Scott Roberts <roberts.moto@gmail.com>
2021-12-27 09:15:16 +0100

    working on graphql schema

--- src/graphqlapp/schema.py
+++ src/graphqlapp/schema.py
@@ -180,7 +180,7 @@ class BlogPostNode(DjangoObjectType):
 
     reading_time = graphene.Field(Int, resolver=resolve_reading_time)
 
-    dpy = graphene.Field(String, resolver=resolve_dpy)
+    # dpy = graphene.Field(String, resolver=resolve_dpy)
     # dpm = graphene.Field(String, resolver=resolve_dpm)
     # dpd = graphene.Field(String, resolver=resolve_dpd)
 
