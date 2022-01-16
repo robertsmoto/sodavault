987cc73a51b87f8d470567a683a7dfe005073e83
Scott Roberts <roberts.moto@gmail.com>
2022-01-16 12:41:46 +0100

    working on blogapp models

--- src/blogapp/admin.py
+++ src/blogapp/admin.py
@@ -50,7 +50,9 @@ class PostAdmin(admin.ModelAdmin):
     fields = [
         ("locations", "post_type"),
         ("title", "slug"),
-        ("parent", "menu_order", "primary_menu"),
+        (
+            "parent", "menu_order", "is_primary_menu", "is_secondary_menu",
+            "is_footer_menu"),
         ("author"),
         ("status", "featured"),
         ("date_published", "date_modified"),
