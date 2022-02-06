c991a0dd27aed8e4500d53b8cca0e08897ceab55
Scott Roberts <roberts.moto@gmail.com>
2022-02-06 10:46:26 +0100

    adding test field

--- src/itemsapp/models.py
+++ src/itemsapp/models.py
@@ -104,7 +104,7 @@ class AttributeManager(models.Manager):
 
 class Attribute(Group):
     # new_field_two = models.CharField(max_length=200, blank=True)
-    # new_field = models.CharField(max_length=200, blank=True)
+    new_field = models.CharField(max_length=200, blank=True)
     objects = AttributeManager
 
     class Meta:
