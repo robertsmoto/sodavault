1068e363d8b1f49fe37087e6d71cfc5dc50853ab
Scott Roberts <roberts.moto@gmail.com>
2022-02-10 15:15:32 +0100

    working on schema

--- src/blogapp/schema.py
+++ src/blogapp/schema.py
@@ -53,11 +53,11 @@ class PostNode(DjangoObjectType):
     class Meta:
         model = blogapp.models.Post
         filter_fields = {
-                'categories__id': ['exact'],
-                'categories__slug': ['exact'],
-                'tags__id': ['exact'],
-                'tags__slug': ['exact'],
-                'locations__domain': ['exact'],
+                # 'categories__id': ['exact'],
+                # 'categories__slug': ['exact'],
+                # 'tags__id': ['exact'],
+                # 'tags__slug': ['exact'],
+                # 'locations__domain': ['exact'],
                 'is_footer_menu': ['exact'],
                 'is_primary_menu': ['exact'],
                 'is_secondary_menu': ['exact'],
