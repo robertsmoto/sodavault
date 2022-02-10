12778f96bc75b0002e87848bc3ab6b47fba6216c
Scott Roberts <roberts.moto@gmail.com>
2022-02-10 13:30:28 +0100

    updating schema and models

--- src/blogapp/schema.py
+++ src/blogapp/schema.py
@@ -1,4 +1,4 @@
-from blogapp.models import Category, Tag, Post
+from blogapp.models import Post  # Category, Tag,
 # from configapp.models import Location
 from blogapp.models import Ingredient, Recipe
 from blogapp.models import LocalBusiness, Book, Movie
@@ -83,42 +83,42 @@ class UserNode(DjangoObjectType):
         # interfaces = (relay.Node, )
 
 
-class CategoryNode(DjangoObjectType):
-    class Meta:
-        model = Category
-        filter_fields = [
-                'id',
-                'slug',
-                'name',
-                'is_primary_menu',
-                'is_secondary_menu',
-                'is_footer_menu',
-                # 'locations__domain',
-                ]
+# class CategoryNode(DjangoObjectType):
+    # class Meta:
+        # model = Category
+        # filter_fields = [
+                # 'id',
+                # 'slug',
+                # 'name',
+                # 'is_primary_menu',
+                # 'is_secondary_menu',
+                # 'is_footer_menu',
+                # # 'locations__domain',
+                # ]
 
-        interfaces = (relay.Node, )
+        # interfaces = (relay.Node, )
 
 
-class TagNode(DjangoObjectType):
-    class Meta:
-        model = Tag
-        filter_fields = [
-                'id',
-                'slug',
-                'name',
-                # 'locations__domain',
-                ]
-        interfaces = (relay.Node, )
+# class TagNode(DjangoObjectType):
+    # class Meta:
+        # model = Tag
+        # filter_fields = [
+                # 'id',
+                # 'slug',
+                # 'name',
+                # # 'locations__domain',
+                # ]
+        # interfaces = (relay.Node, )
 
 
 class PostNode(DjangoObjectType):
     class Meta:
         model = Post
         filter_fields = {
-                'categories__id': ['exact'],
-                'categories__slug': ['exact'],
-                'tags__id': ['exact'],
-                'tags__slug': ['exact'],
+                # 'categories__id': ['exact'],
+                # 'categories__slug': ['exact'],
+                # 'tags__id': ['exact'],
+                # 'tags__slug': ['exact'],
                 # 'locations__domain': ['exact'],
                 'is_footer_menu': ['exact'],
                 'is_primary_menu': ['exact'],
@@ -211,11 +211,11 @@ class Query(graphene.ObjectType):
     # location = relay.Node.Field(LocationNode)
     # all_locations = DjangoFilterConnectionField(LocationNode)
 
-    category = relay.Node.Field(CategoryNode)
-    all_categories = DjangoFilterConnectionField(CategoryNode)
+    # category = relay.Node.Field(CategoryNode)
+    # all_categories = DjangoFilterConnectionField(CategoryNode)
 
-    tag = relay.Node.Field(TagNode)
-    all_tags = DjangoFilterConnectionField(TagNode)
+    # tag = relay.Node.Field(TagNode)
+    # all_tags = DjangoFilterConnectionField(TagNode)
 
     posts = relay.Node.Field(PostNode)
     all_posts = DjangoFilterConnectionField(PostNode)
