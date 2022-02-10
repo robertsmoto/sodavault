45f16e0f085506393cf4f9254ec9a4c684503375
Scott Roberts <roberts.moto@gmail.com>
2022-02-10 14:25:00 +0100

    updating schema and models

--- src/blogapp/schema.py
+++ src/blogapp/schema.py
@@ -1,5 +1,5 @@
 from blogapp.models import Post  # Category, Tag,
-from configapp.models import Location
+from configapp.models import Location, Group
 from blogapp.models import Ingredient, Recipe
 from blogapp.models import LocalBusiness, Book, Movie
 from blogapp.models import OpeningHours, Review
@@ -83,32 +83,35 @@ class LocationNode(DjangoObjectType):
         interfaces = (relay.Node, )
 
 
-# class CategoryNode(DjangoObjectType):
-    # class Meta:
-        # model = Category
-        # filter_fields = [
-                # 'id',
-                # 'slug',
-                # 'name',
-                # 'is_primary_menu',
-                # 'is_secondary_menu',
-                # 'is_footer_menu',
-                # # 'locations__domain',
-                # ]
+class CategoryNode(DjangoObjectType):
+    class Meta:
+        model = Group
+        filter_fields = [
+                'id',
+                'slug',
+                'name',
+                'is_primary_menu',
+                'is_secondary_menu',
+                'is_footer_menu',
+                'locations__domain',
+                ]
 
-        # interfaces = (relay.Node, )
+        interfaces = (relay.Node, )
 
 
-# class TagNode(DjangoObjectType):
-    # class Meta:
-        # model = Tag
-        # filter_fields = [
-                # 'id',
-                # 'slug',
-                # 'name',
-                # # 'locations__domain',
-                # ]
-        # interfaces = (relay.Node, )
+class TagNode(DjangoObjectType):
+    class Meta:
+        model = Group
+        filter_fields = [
+                'id',
+                'slug',
+                'name',
+                'is_primary_menu',
+                'is_secondary_menu',
+                'is_footer_menu',
+                'locations__domain',
+                ]
+        interfaces = (relay.Node, )
 
 
 class PostNode(DjangoObjectType):
@@ -211,11 +214,11 @@ class Query(graphene.ObjectType):
     location = relay.Node.Field(LocationNode)
     all_locations = DjangoFilterConnectionField(LocationNode)
 
-    # category = relay.Node.Field(CategoryNode)
-    # all_categories = DjangoFilterConnectionField(CategoryNode)
+    category = relay.Node.Field(CategoryNode)
+    all_categories = DjangoFilterConnectionField(CategoryNode)
 
-    # tag = relay.Node.Field(TagNode)
-    # all_tags = DjangoFilterConnectionField(TagNode)
+    tag = relay.Node.Field(TagNode)
+    all_tags = DjangoFilterConnectionField(TagNode)
 
     posts = relay.Node.Field(PostNode)
     all_posts = DjangoFilterConnectionField(PostNode)
