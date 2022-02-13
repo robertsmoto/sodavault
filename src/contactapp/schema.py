c748017b0a8ecd1a76e81c02dafe851b32cc527f
Scott Roberts <roberts.moto@gmail.com>
2022-02-13 12:20:54 +0100

    working on schema

--- src/contactapp/schema.py
+++ src/contactapp/schema.py
@@ -5,9 +5,9 @@ import configapp.models
 import graphene
 
 
-class CompanyNode(DjangoObjectType):
+class LocationNode(DjangoObjectType):
     class Meta:
-        model = configapp.models.Company
+        model = configapp.models.Location
         filter_fields = [
                 'domain',
                 'name',
@@ -15,42 +15,54 @@ class CompanyNode(DjangoObjectType):
         interfaces = (relay.Node, )
 
 
-class StoreNode(DjangoObjectType):
-    class Meta:
-        model = configapp.models.Store
-        filter_fields = [
-                'domain',
-                'name',
-                ]
-        interfaces = (relay.Node, )
+# class CompanyNode(DjangoObjectType):
+    # class Meta:
+        # model = configapp.models.Company
+        # filter_fields = [
+                # 'domain',
+                # 'name',
+                # ]
+        # interfaces = (relay.Node, )
 
 
-class WarehouseNode(DjangoObjectType):
-    class Meta:
-        model = configapp.models.Warehouse
-        filter_fields = [
-                'domain',
-                'name',
-                ]
-        interfaces = (relay.Node, )
+# class StoreNode(DjangoObjectType):
+    # class Meta:
+        # model = configapp.models.Store
+        # filter_fields = [
+                # 'domain',
+                # 'name',
+                # ]
+        # interfaces = (relay.Node, )
 
 
-class WebsiteNode(DjangoObjectType):
-    class Meta:
-        model = configapp.models.Website
-        filter_fields = [
-                'domain',
-                'name',
-                ]
-        interfaces = (relay.Node, )
+# class WarehouseNode(DjangoObjectType):
+    # class Meta:
+        # model = configapp.models.Warehouse
+        # filter_fields = [
+                # 'domain',
+                # 'name',
+                # ]
+        # interfaces = (relay.Node, )
+
+
+# class WebsiteNode(DjangoObjectType):
+    # class Meta:
+        # model = configapp.models.Website
+        # filter_fields = [
+                # 'domain',
+                # 'name',
+                # ]
+#         interfaces = (relay.Node, )
 
 
 class Query(graphene.ObjectType):
-    company = relay.Node.Field(CompanyNode)
-    all_companies = DjangoFilterConnectionField(CompanyNode)
-    store = relay.Node.Field(StoreNode)
-    all_stores = DjangoFilterConnectionField(StoreNode)
-    warehouse = relay.Node.Field(WarehouseNode)
-    all_warehouses = DjangoFilterConnectionField(WarehouseNode)
-    website = relay.Node.Field(WebsiteNode)
-    all_websites = DjangoFilterConnectionField(WebsiteNode)
+    location = relay.Node.Field(LocationNode)
+    all_locations = DjangoFilterConnectionField(LocationNode)
+#     company = relay.Node.Field(CompanyNode)
+    # all_companies = DjangoFilterConnectionField(CompanyNode)
+    # store = relay.Node.Field(StoreNode)
+    # all_stores = DjangoFilterConnectionField(StoreNode)
+    # warehouse = relay.Node.Field(WarehouseNode)
+    # all_warehouses = DjangoFilterConnectionField(WarehouseNode)
+    # website = relay.Node.Field(WebsiteNode)
+    # all_websites = DjangoFilterConnectionField(WebsiteNode)
