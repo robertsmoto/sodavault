c217be46da835400fb8afe2f4f0b4d0d7d101c67
Scott Roberts <roberts.moto@gmail.com>
2022-02-18 17:36:38 +0100

    workign on models

--- src/blogapp/migrations/0023_auto_20220218_1557.py
+++ src/blogapp/migrations/0023_auto_20220218_1557.py
@@ -9,24 +9,24 @@ class Migration(migrations.Migration):
         ('blogapp', '0022_alter_post_websites'),
     ]
 
-    operations = [
-        migrations.CreateModel(
-            name='Website',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(blank=True, max_length=200)),
-                ('description', models.CharField(blank=True, max_length=200)),
-                ('domain', models.CharField(blank=True, max_length=200, verbose_name='Domain eg. example.com')),
-                ('website', models.CharField(blank=True, max_length=200)),
-            ],
-            options={
-                'verbose_name_plural': 'locations',
-                'ordering': ['domain'],
-            },
-        ),
-        migrations.AlterField(
-            model_name='post',
-            name='websites',
-            field=models.ManyToManyField(blank=True, to='blogapp.Website'),
-        ),
-    ]
+    # operations = [
+        # migrations.CreateModel(
+            # name='Website',
+            # fields=[
+                # ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                # ('name', models.CharField(blank=True, max_length=200)),
+                # ('description', models.CharField(blank=True, max_length=200)),
+                # ('domain', models.CharField(blank=True, max_length=200, verbose_name='Domain eg. example.com')),
+                # ('website', models.CharField(blank=True, max_length=200)),
+            # ],
+            # options={
+                # 'verbose_name_plural': 'locations',
+                # 'ordering': ['domain'],
+            # },
+        # ),
+        # migrations.AlterField(
+            # model_name='post',
+            # name='websites',
+            # field=models.ManyToManyField(blank=True, to='blogapp.Website'),
+        # ),
+    # ]
