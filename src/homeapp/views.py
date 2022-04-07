26c9357936ec0d0aa23f3c548778b4415d49d807
Scott Roberts <roberts.moto@gmail.com>
2022-04-07 22:04:27 +0200

    updating blogapp models

--- src/homeapp/views.py
+++ src/homeapp/views.py
@@ -11,6 +11,8 @@ class HomeView(MetaData, BrCrumb, Navigation, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        queryset = Page.objects.get_or_create(slug='home')
+        queryset, created = Page.objects.get_or_create(slug='home')
+        if created:
+            print("###created new Page with slug 'home'")
         context['object'] = queryset
         return context
